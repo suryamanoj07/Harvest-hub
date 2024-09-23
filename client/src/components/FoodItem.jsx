b5909badbb9dadabf0c38109f20a4d73fa470fe6
suryamanoj07 <manojsurya463@gmail.com>
2024-09-24 02:06:45 +0530

    updated user products

--- client/src/components/FoodItem.jsx
+++ client/src/components/FoodItem.jsx
@@ -22,10 +22,10 @@ const FoodItem = ({id,name,price,description,image}) => {
         </div>    
         <div className="food-item-info">
             <div className="food-item-name-rating">
-                <p>{name}</p>
+                <p className='h-8 m-2'>{name}</p>
                 <img src={assets.rating_starts} alt="" />
             </div>
-            <p className="food-item-desc">{description}</p>
+            <p className="food-item-desc h-16">{description}</p>
             <p className="food-item-price">Rs. {price}/-</p>
         </div>
     </div>
